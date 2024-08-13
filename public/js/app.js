36753e249db322ae666139cf937c5b51dd9b1490
Dmitry <melkov.dmitriy40@gmail.com>
2024-08-13 09:57:57 +0500

    add temper from frame result

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
-console.log('yo');
+window.addEventListener('message', (event) => {
+  if (event.origin !== 'http://techsite4') {
+    return;
+  }
+
+  const message = event.data;
+  const temperatureTypes = {
+    temperatureValue1: 'temperatureValue1',
+    temperatureValue2: 'temperatureValue2',
+    temperatureValue3: 'temperatureValue3',
+  };
+
+  if (temperatureTypes[message.type]) {
+    if (message.value !== null) {
+      const temperature = message.value;
+      // Создаем переменную для каждого типа температуры и выводим в консоль
+      const temperatureValue = temperature;
+      console.log(`Переменная ${temperatureTypes[message.type]}:`, temperatureValue);
+    }
+  }
+});
--- src/server/index.js
+++ src/server/index.js
@@ -3,17 +3,14 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 92;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 app.use(express.static(path.join(__dirname, '../../public')));
-
 
 // Запуск сервера
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://169.254.7.86:${PORT}`);
 });
