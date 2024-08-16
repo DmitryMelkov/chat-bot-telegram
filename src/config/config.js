fd460fcd928efdfd15645ec6e4bfd6ccb6064dc7
Dmitry <melkov.dmitriy40@gmail.com>
2024-08-16 14:32:23 +0500

    add alarms buttons and content

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,7 @@ dotenv.config(); // Загружаем переменные из .env
 
 export const config = {
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+  CHAT_ID: process.env.CHAT_ID,
   PROXY_URL: 'http://169.254.0.51:3274',
   PORT: 92,
 };
--- src/telegram-bot/buttonHandlers.js
+++ src/telegram-bot/buttonHandlers.js
@@ -2,6 +2,7 @@
 import { sendMessageWithButtons } from './sendMessage.js';
 import { editMessageWithButtons } from './editMessage.js';
 import { generateTablePechVr } from './generateTable.js';
+import { checkAndNotify } from './alarms.js';
 
 export const handleMessage = (bot, chatId) => {
   sendMessageWithButtons(bot, chatId, 'Выберите интересующую опцию:', [
@@ -18,10 +19,12 @@ export const handleCallbackQuery = (bot, app, query) => {
   const buttons = {
     furnace_1: [
       [{ text: 'Текущие параметры', callback_data: 'get_temperature_1' }],
+      [{ text: 'Алармы', callback_data: 'check_alarms_1' }], // Добавляем кнопку Алармы
       [{ text: 'Назад', callback_data: 'production_carbon' }],
     ],
     furnace_2: [
       [{ text: 'Текущие параметры', callback_data: 'get_temperature_2' }],
+      [{ text: 'Алармы', callback_data: 'check_alarms_2' }], // Добавляем кнопку Алармы
       [{ text: 'Назад', callback_data: 'production_carbon' }],
     ],
     production_carbon: [
@@ -38,9 +41,13 @@ export const handleCallbackQuery = (bot, app, query) => {
     const furnaceNumber = action === 'get_temperature_1' ? 1 : 2;
     const table = generateTablePechVr(data, furnaceNumber, currentTime);
     editMessageWithButtons(bot, chatId, query.message.message_id, table, [
+      [{ text: 'Алармы', callback_data: `check_alarms_${furnaceNumber}` }], // Кнопка Алармы
       [{ text: 'Назад', callback_data: `furnace_${furnaceNumber}` }],
       [{ text: 'Обновить данные', callback_data: action }],
     ]);
+  } else if (action.startsWith('check_alarms_')) {
+    const furnaceNumber = action === 'check_alarms_1' ? 1 : 2;
+    checkAndNotify(data, bot, chatId, furnaceNumber, query.message.message_id); // Передаем message_id для обновления
   } else {
     const buttonSet = buttons[action] || buttons.back_to_production;
     sendMessageWithButtons(bot, chatId, 'Выберите интересующую опцию:', buttonSet);
