06df975e2e3dfde553948151ee31514a51715a50
Dmitry <melkov.dmitriy40@gmail.com>
2024-08-20 09:40:12 +0500

    add fix restart

--- src/server.js
+++ src/server.js
@@ -2,15 +2,14 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import mongoose from 'mongoose';
-import { config } from './config/config.js';
-import { updateValuesRoute } from './routes/updateValues.js';
-import createTelegramBot from './telegram-bot/telegramBot.js';
-import { logErrorAndRestart } from './controllers/serverControl.js'; // Импортируем функцию
+import { config } from './config/config.js'; // Конфигурация приложения
+import { updateValuesRoute } from './routes/updateValues.js'; // Маршрут для обновления значений
+import createTelegramBot from './telegram-bot/telegramBot.js'; // Функция создания Telegram бота
 
 const app = express();
 const PORT = config.PORT;
 
-// Определение __filename и __dirname
+// Определение __dirname и __filename
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -20,7 +19,7 @@ mongoose.connect('mongodb://localhost:27017/furnaceData')
     console.log('Connected to MongoDB');
   })
   .catch((err) => {
-    logErrorAndRestart('Error connecting to MongoDB', err);
+    console.error('Error connecting to MongoDB', err);
   });
 
 // Настройка статического сервера для файлов в public
@@ -28,23 +27,10 @@ app.use(express.static(path.join(__dirname, '../public/')));
 app.use(express.json());
 
 // Создание и настройка Telegram бота
-const bot = createTelegramBot(app);
+const bot = createTelegramBot(app); // Создаем бота и передаем Express приложение
 
 // Подключаем маршруты
 updateValuesRoute(app);
-
-// Обработка ошибок polling_error
-bot.on('polling_error', (error) => {
-  logErrorAndRestart('Polling error detected', error);
-});
-
-// Автоматический перезапуск сервера раз в час
-setInterval(() => {
-  const timeStamp = new Date().toLocaleString();
-  console.log(`[${timeStamp}] Scheduled server restart`);
-
-  logErrorAndRestart('Scheduled restart');
-}, 60 * 60 * 1000);
 
 // Запуск сервера
 app.listen(PORT, () => {
